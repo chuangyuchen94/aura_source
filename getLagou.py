90cbafe6c8b492feda7ee8ea8153bfa763bd4c9f
chuangyuchen94 <chuangyuchen94@gmail.com>
2025-02-26 16:38:01 +0800

    修改代码，增加输出到文件

--- getLagou.py
+++ getLagou.py
@@ -20,7 +20,7 @@ def get_headers(page_no):
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
         "host": "www.lagou.com",
         "referer": f"https://www.lagou.com/wn/zhaopin?fromSearch=true&kd=%25E4%25BA%25BA%25E5%25B7%25A5%25E6%2599%25BA%25E8%2583%25BD&pn={page_no-1}",
-        "cookie": """index_location_city=%E5%85%A8%E5%9B%BD; RECOMMEND_TIP=1; JSESSIONID=ABAABJAABAJACEJ7CF173DD21E5C6D072846A49E9C5B120; WEBTJ-ID=20250223225942-19533523d0992-0ed8590a8f5ac5-1c525636-1440000-19533523d0a19ce; sensorsdata2015session=%7B%7D; user_trace_token=20250223225949-8b7149ed-504d-4610-a00a-1360cb15db5a; LGUID=20250223225949-e365e8be-348a-415b-9fe2-eb1faf2f33e1; _ga=GA1.2.158572295.1740322789; LGRID=20250223230202-bb00f215-2ae3-491e-92fc-369c45abd68c; _ga_DDLTLJDLHH=GS1.2.1740322790.1.1.1740322922.43.0.0; gate_login_token=v1####d1522b0f665a797c9234afd8040e68eb43ae38de967271d000601421235b8951; LG_HAS_LOGIN=1; _putrc=576A121222766EE5123F89F2B170EADC; login=true; hasDeliver=0; privacyPolicyPopup=false; __lg_stoken__=62a94fa51dc8491ba41f3a0642284ceaa1b2d6029648df44683a4bfbded2b33b9c5b67f187a6c09bb9bacfaae0e962c2fe2d66899ca0766bcbbb9bdb2355b88c35d96a5d03b6; unick=%E7%94%A8%E6%88%B75134; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; X_HTTP_TOKEN=62bb8932f865e8c37453940471063cec905923f5e8; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2219533523dad1c3-0730b3fa3249fa-1c525636-1440000-19533523dae2af9%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_utm_source%22%3A%22PC_SEARCH%22%2C%22%24os%22%3A%22MacOS%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%22133.0.0.0%22%7D%2C%22%24device_id%22%3A%2219533523dad1c3-0730b3fa3249fa-1c525636-1440000-19533523dae2af9%22%7D""",
+        "cookie": """index_location_city=%E5%85%A8%E5%9B%BD; RECOMMEND_TIP=1; _ga=GA1.2.201396242.1740444374; user_trace_token=20250225084619-e57426b2-f845-42c9-a6da-19ee0059fe6b; LGUID=20250225084619-645f68bf-d09f-40dc-804c-9445ed042b77; _ga_DDLTLJDLHH=GS1.2.1740444385.1.0.1740444385.60.0.0; gate_login_token=v1####d1522b0f665a797c9234afd8040e68eb43ae38de967271d000601421235b8951; LG_HAS_LOGIN=1; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=0; privacyPolicyPopup=false; JSESSIONID=ABAABJAACBBAAHG420C6ACDA3B30D68AF4ACEDCF09AAC34; WEBTJ-ID=02262025%2C084858-1953fba712119-0a5a6999686681-26011a51-1382400-1953fba712211a1; sensorsdata2015session=%7B%7D; LG_LOGIN_USER_ID=v1####4f68d128b18bc548ccbfbcce920bb01ef4a619ad978a6e9bd5d05554534573b7; _putrc=576A121222766EE5123F89F2B170EADC; login=true; unick=%E9%99%88%E6%A0%91%E5%BD%A6; X_HTTP_TOKEN=c4bd1fdfd8d3d9ae86903504715fbce1d9940cb566; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221953a917a292e8-03963bac7d5d67-26011a51-1382400-1953a917a2a32d%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24os%22%3A%22Windows%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%22133.0.0.0%22%7D%2C%22%24device_id%22%3A%221953a917a292e8-03963bac7d5d67-26011a51-1382400-1953a917a2a32d%22%7D2""",
     }
 
     return headers
@@ -55,6 +55,22 @@ def get_job_info(page_no):
 
     return jost_details
 
+def get_all_job_info(max_page=0):
+    with open("job_info.txt", mode="w", encoding="utf8") as job_info_file:
+        page_no = 1
+
+        while True:
+            if max_page != 0 and page_no > max_page:
+                break
+
+            job_infos = get_job_info(page_no)
+            if len(job_infos) == 0:
+                break
+
+            page_no += 1
+
+            for job_detail in job_infos:
+                job_info_file.write(f"{job_detail.get("position_name")}\t{job_detail.get("money")}\t{job_detail.get("origin")}\t{job_detail.get("distinct")}\t{job_detail.get("experience_text")}\n")
+
 if __name__ == '__main__':
-    jost_details = get_job_info(1)
-    print(jost_details)
+    get_all_job_info(max_page=1)
