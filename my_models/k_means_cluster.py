9e5e0b90c00995ed59c123a0493a5548a9520a31
chuangyuchen94 <chuangyuchen94@gmail.com>
2025-03-26 20:05:16 +0800

    新增：KMEANS算法的代码实现框架

---
+++ my_models/k_means_cluster.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class MyKMeansCluster:
+    """
+    实现K-MEANS聚类算法
+    """
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
