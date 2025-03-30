547bc7570497b6ecfeebe40238fc4d9fbf676496
“chuangyuchen94” <chuangyuchen94@gmail.com>
2025-03-30 11:10:51 +0800

    修改：将图像进行分割，并保存为图像

--- test/ladybug_cut_demo.py
+++ test/ladybug_cut_demo.py
@@ -26,19 +26,42 @@ def split_image(img_array, part_num=2):
     img_parts = []
     labels = np.unique(img_model.labels_)
     for label in labels:
-        single_part = img_part_label[img_model.labels_ == label].copy()
+        single_part = img_array.copy()
+        single_part[img_model.labels_ != label] = 255 # 其他区域置为白色
         img_parts.append(single_part)
 
     return img_parts
 
+def save_image(image_data, file_shape, file_path):
+    """
+    将图像的二维数据，写回变成图像
+    :param image_data: 二维数组
+    :param file_path: 原始文件的路径
+    :return:
+    """
+    for image_index, image in enumerate(image_data):
+        image_file_name = get_new_file_path(file_path, image_index)
+        image_3d_data = image.reshape(file_shape)
+        Image.fromarray(image_3d_data).save(image_file_name)
+
+def get_new_file_path(file_path, num=0):
+    file_postfix = file_path.rfind(".")
+    new_file_path = f"{file_path[:file_postfix]}{num}{file_path[file_postfix:]}"
+    print(f"new_file_path: {new_file_path}")
+
+    return new_file_path
+
 if "__main__" == __name__:
     img_path = "../data/ladybug.png"
     img_data = load_image_data(img_path)
+    image_shape = img_data.shape
     print(img_data.shape)
+    get_new_file_path(img_path, 0)
+    get_new_file_path(img_path, 1)
 
     img_data = img_data.reshape(-1, 3)
 
-    img_part_all = split_image(img_data, part_num=2)
+    img_part_all = split_image(img_data, part_num=3)
     print(len(img_part_all))
-    print(len(img_part_all))
+    save_image(img_part_all, image_shape, img_path)
 
