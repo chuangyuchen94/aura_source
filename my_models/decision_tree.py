2b24586984d9070e66906dba623627df535797d7
chuangyuchen94 <chuangyuchen94@gmail.com>
2025-03-31 18:06:17 +0800

    修改：决策树的实现

--- my_models/decision_tree.py
+++ my_models/decision_tree.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def create_dataset():
     data_set = [
         [0, 0, 0, 0, "no"],
@@ -20,5 +20,67 @@ def create_dataset():
 
     labels = ["F1-AGE", "F2-WORK", "F3-HOME", "F4-LOAN"]
 
-    return data_set, labels
+    return np.array(data_set), np.array(labels)
 
+
+class MyDecisionTree:
+    """
+    决策树模型
+    """
+    def __init__(self):
+        self.features_used = [] # 已经使用的特征
+        pass
+
+    def fit(self, X, y):
+        """
+        训练方法
+        1、每次分割的操作：要按照分割条件将数据分割
+        :param X:
+        :param y:
+        :return:
+        """
+        # 找到没被使用的特征，并计算其熵值
+        feature_all = range(X.shape[1])
+        featured_unused = set(feature_all) - set(self.features_used)
+
+        for feature in featured_unused:
+            self.calc_feature_entropy(X, y, feature)
+
+
+    def calc_feature_entropy(self, X, y, feature_index):
+        """
+        计算节点的熵值
+        :param X:
+        :param y:
+        :return:
+        """
+        # 按照枚举值或连续值来切割
+        entropy_value = 0
+
+        feature_value = np.unique(X[:, feature_index])
+        for feature in feature_value:
+            y_slice = y[X[:, feature_index] == feature]
+            y_slice_entropy = MyDecisionTree.calc_target_entropy(y_slice)
+            entropy_value += y_slice_entropy
+
+    @staticmethod
+    def calc_target_entropy(y):
+        """
+        计算目标数据集的熵值
+        :param y:
+        :return:
+        """
+        y_value = np.unique(y)
+        y_entropy_value = sum([(sum(y == y_label)/len(y) * np.log2(sum(y == y_label)/len(y))) for y_label in y_value])
+        return y_entropy_value
+
+    def predict(self, X):
+        """
+        预测方法
+        :param X:
+        :return:
+        """
+
+if "__main__" == __name__:
+    data, label = create_dataset()
+    print(f"data: {data.shape}")
---
+++ test/decision_tree_demo.py
@@ -0,0 +1,13 @@
+from my_models.decision_tree import create_dataset, MyDecisionTree
+from sklearn.model_selection import train_test_split
+
+data, label = create_dataset()
+print(f"data: {data.shape}")
+
+X_all = data[:, :-1]
+y_all = data[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.2, random_state=42)
+
+tree_model = MyDecisionTree()
+tree_model.fit(X_train, y_train)
+tree_model.predict(X_test)
