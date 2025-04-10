646c8b7769f16bde3195bb1902727223c5fcba40
chuangyuchen94 <chuangyuchen94@gmail.com>
2025-04-10 22:04:26 +0800

    修改： 1、修改：补充隐藏层按类来实现的代码

--- my_models/neural_network.py
+++ my_models/neural_network.py
@@ -13,7 +13,7 @@ class NeuralLayer:
     2）reverse_brocast：反向传播
     3）update_theta：更新参数
     """
-    def __init__(self, input_num, output_num, activation_func=None, derivative_func=None, is_input=False, is_final_outcome=False):
+    def __init__(self, input_num, output_num, activation_func=None, derivative_func=None, is_input=False, is_final_outcome=False, last_activation_func=None, momentum=0.9):
         self.input_num = input_num # 输入的维度
         self.output_num = output_num # 输出的维度
         self.activation_func = activation_func # 激活函数
@@ -23,6 +23,10 @@ class NeuralLayer:
         self.y = None # 输出值
         self.theta = None # 参数
         self.is_final_outcome = is_final_outcome # 激活层计算的结果，是否就是最终结果
+        self.last_activation_func = last_activation_func # 最后一层的激活函数，即输出层的前一层隐藏层的激活函数
+        self.momentum = momentum
+        self.velocity = 0  # 初始化velocity
+
 
     def init_theta(self):
         """
@@ -38,30 +42,37 @@ class NeuralLayer:
         :param input_value:
         :return:
         """
-        input_features = np.insert(X, 0, 1, axis=1) if self.is_input else X
+        y_result_linear = X.dot(self.theta)  # 线性变换
 
-        y_result_linear = input_features.dot(self.theta)  # 线性变换
-        y_result_activate = self.activation_func(y_result_linear)
+        #激活函数处理
+        if self.is_final_outcome:
+            y_result_activate = self.last_activation_func(y_result_linear)
+        else:
+            y_result_activate = self.activation_func(y_result_linear)
 
-        self.a = input_features.copy()  # 输入值
+        self.a = X.copy()  # 输入值
         self.y = y_result_activate.copy()  # 输出值
         activate_mean = y_result_activate.mean()
 
         return y_result_activate, activate_mean
 
-    def reverse_broacast(self, layer_index, loss_of_next_layer, theta_of_next_layer, learning_rate, print_log=False):
+    def reverse_broadcast(self, layer_index, loss_of_next_layer, theta_of_next_layer, learning_rate, print_log=False):
         """
         反向传播
         :return:
         """
+        theta_of_current_layer = self.theta.copy()
+
         if not self.is_final_outcome:
             deriv_value = self.derivative_func(self.y)
             loss_of_next_layer = (loss_of_next_layer.dot(theta_of_next_layer.T)) * deriv_value
 
         input_of_current_layer = self.a # 本层的输入值
-        gradient_value = input_of_current_layer.T.dot(loss_of_next_layer) / \
-                                        input_of_current_layer.shape[0]
-        self.theta -= learning_rate * gradient_value
+        gradient_value = input_of_current_layer.T.dot(loss_of_next_layer) / input_of_current_layer.shape[0]
+        velocity = self.momentum * self.velocity + gradient_value
+        self.theta -= learning_rate * velocity
+
+        self.velocity = velocity
 
         # 正常范围应在1e-5到1e-3之间
         if print_log and (
@@ -74,7 +85,7 @@ class NeuralLayer:
         if print_log:
             print(f"Layer {layer_index} gradient norm:", grad_norm)
 
-        return grad_norm
+        return theta_of_current_layer, grad_norm
 
     def set_final_outcome_flag(self, flag=True):
         """
@@ -105,12 +116,32 @@ class NeuralLayer:
         """
         return self.theta
 
+    def calc_loss(self, y_pred, y_true, loss_of_next_layer, theta_of_next_layer, print_log=False):
+        """
+        计算当前层的损失
+        :param y_std:
+        :param loss_of_next_layer:
+        :param theta_of_next_layer:
+        :param print_log:
+        :return:
+        """
+        loss_value = None
+
+        if not self.is_final_outcome:
+            deriv_value = self.derivative_func(self.y)
+            loss_value = (loss_of_next_layer.dot(theta_of_next_layer.T)) * deriv_value
+        else:
+            loss_value = y_pred - y_true
+
+        return loss_value
+
 class MyNeuralNetwork:
     """
     实现神经网络算法
     1、 层与层之间的数据处理：线性变换、激活函数
     """
-    def __init__(self, max_iter=1000, tol=1e-5, learning_rate=0.01, layer=None, y_to_one_hot=True, theta_init_rate =0.01, print_log=False, random_state=42):
+    def __init__(self, max_iter=1000, tol=1e-5, learning_rate=0.01, layer=None, y_to_one_hot=True, theta_init_rate =0.01, print_log=False, momentum=0.9, random_state=42):
+        self.layer_of_all = None
         if layer is None:
             layer = [25, ]
         self.max_iter = max_iter # 最大迭代次数
@@ -127,6 +158,8 @@ class MyNeuralNetwork:
         self.grad_norm = []
         self.activation_mean = []
         self.print_log = print_log
+        self.momentum = momentum
+        self.velocities = {}  # 初始化velocity字典
 
     def fit(self, X, y):
         X_std = self.standardize(X)
@@ -135,17 +168,16 @@ class MyNeuralNetwork:
         y_label_num = len(np.unique(y))
 
         self.init_layer(self.layer, X_input, y_label_num)
+        self.init_thetas()
 
         loss_of_iter = []
 
         for _ in range(self.max_iter):
-            y_result = self.predict_forword(X_std, theta_in_layer) # 前向传播，计算结果
-            theta_in_layer = self.update_thetas_backward(y_result, y_std, X_input, theta_in_layer) # 反向传播，更新参数
+            y_result = self.predict_forward(X_input) # 前向传播，计算结果
+            loss_value = MyNeuralNetwork.calc_loss(y_result, y_std)  # 计算最终的损失值
+            self.update_thetas_backward(y_result, y_std) # 反向传播，更新参数
 
-            loss_value = MyNeuralNetwork.calc_loss(y_result, y_std)
             loss_of_iter.append(loss_value)
-
-        self.theta_in_layer = theta_in_layer
 
         return loss_of_iter
 
@@ -179,8 +211,7 @@ class MyNeuralNetwork:
         layer_num = len(layer) + 1
         layer_of_all = {}
 
-        neural_num_of_layer = []
-        neural_num_of_layer.append(X_input.shape[1])
+        neural_num_of_layer = [X_input.shape[1]]
         neural_num_of_layer.extend(layer)
         neural_num_of_layer.append(y_label_num)
 
@@ -193,65 +224,45 @@ class MyNeuralNetwork:
                 activation_func=MyNeuralNetwork.relu,
                 derivative_func=MyNeuralNetwork.relu_derivative,
                 is_input=layer_index == 0,
-                is_final_outcome=layer_index == layer_num - 1)
+                is_final_outcome=layer_index == layer_num - 1,
+                last_activation_func=MyNeuralNetwork.softmax)
             layer_of_all[layer_index] = layer_of_each
 
-    def init_thetas(self, num_feature, num_label):
+        self.layer_of_all = layer_of_all
+
+    def init_thetas(self):
         """
         初始化每一层的参数值
         :return:
         """
-        theta_num = len(self.layer) + 1 # 1为输入层到隐藏层的参数
-        layer_thetas = {}
-        layer_neural_num = [num_feature, ]
-        layer_neural_num.extend(self.layer)
-        layer_neural_num.append(num_label) # 每一层的神经元个数（特征个数）
-
-        for theta_index in range(theta_num):
-            in_nueral_num = layer_neural_num[theta_index]+ 1 # 1代表偏置项
-            out_nueral_num = layer_neural_num[theta_index + 1]
-            scale = np.sqrt(2.0 / layer_neural_num[theta_index])  # He初始化
-            layer_thetas[theta_index] = np.random.randn(in_nueral_num, out_nueral_num) * scale
-
-        return layer_thetas
+        for layer_index in self.layer_of_all:
+            self.layer_of_all[layer_index].init_theta()
 
 
-    def predict_forword(self, X, theta_in_layer):
+    def predict_forward(self, X):
         """
         前向传播，计算结果
         1、 线性变换
         2、激活函数
         :return:预测结果值
         """
-        y_result = None
         # input_features = X / 255.0  # 添加归一化
-        input_features = X
-        num_of_layer = len(self.layer)
+        input_features_current = X
+        y_result_activate = None
 
-        for theta_index in range(num_of_layer):
-            theta = theta_in_layer[theta_index]
-            input_features = np.insert(input_features, 0, 1, axis=1)
-            y_result_linear = input_features.dot(theta) # 线性变换
-            y_result_activate = MyNeuralNetwork.relu(y_result_linear)
+        for layer_index in self.layer_of_all:
+            layer_of_current = self.layer_of_all[layer_index]
+            y_result_activate, activate_mean = layer_of_current.fit(input_features_current)
 
-            self.value_of_layer[theta_index] = y_result_activate.copy()
-
-            input_features = y_result_activate
+            input_features_current = y_result_activate # 当前层的输出（激活值），是下一层的输入
 
             self.activation_mean.append(y_result_activate.mean())
             if self.print_log:
-                print(f"Layer {theta_index} activation mean:", y_result_activate.mean())
+                print(f"Layer {layer_index} activation mean:", y_result_activate.mean())
 
-        # 最后一层隐藏层到输出层，单独处理，因为涉及分类问题，需要对其应用softmax
-        theta = theta_in_layer[num_of_layer]
-        input_features = np.insert(input_features, 0, 1, axis=1)
-        y_result_linear = input_features.dot(theta)  # 线性变换
-        y_result = MyNeuralNetwork.softmax(y_result_linear) # softmax变换
-        self.value_of_layer[num_of_layer] = y_result.copy()
+        return y_result_activate
 
-        return y_result
-
-    def update_thetas_backward(self, y_result, y_std, X_std, theta_in_layer):
+    def update_thetas_backward(self, y_result, y_std):
         """
         反向传播：计算每一层的梯度值，并更新每一层的参数
         :return:
@@ -260,40 +271,20 @@ class MyNeuralNetwork:
         # 先处理最后一层隐藏层到输出层的梯度值、更新参数（因为最后一层到输出层的激活函数用的是softmax，与其他层不同）
         # 计算损失值
         num_of_layer = len(self.layer) + 1
-        theta_in_layer_new = theta_in_layer.copy()
+        loss_of_next_layer = y_result - y_std
+        theta_of_next_layer = None
 
-        outer_layer = num_of_layer - 1
-        outer_loss = self.calc_ouput_loss(y_result, y_std)
-        input_of_outer = self.value_of_layer[outer_layer - 1]
-        input_of_outer_extend = np.insert(input_of_outer, 0, 1, axis=1)
-        outer_gradient = input_of_outer_extend.T.dot(outer_loss)
-        theta_in_layer_new[outer_layer] = theta_in_layer[outer_layer] - self.learning_rate * outer_gradient
+        for layer_index in range(num_of_layer - 1, -1, -1):
+            layer_of_current = self.layer_of_all[layer_index]
+            theta_of_current_layer, grad_norm = layer_of_current.reverse_broadcast(layer_index, loss_of_next_layer, theta_of_next_layer, self.learning_rate, print_log=self.print_log)
 
-        # 处理每一层隐藏层之间：计算损失值->计算梯度值->更新参数
-        loss_of_next_layer = outer_loss
-        for layer_index in range(outer_layer - 1, -1, -1):
-            y_result_activate = self.value_of_layer[layer_index]
-            relu_deriv = MyNeuralNetwork.relu_derivative(y_result_activate)
-            theta_of_next_layer = theta_in_layer.get(layer_index + 1)
-            loss_of_current_layer = (loss_of_next_layer.dot(theta_of_next_layer.T[:, 1:])) * relu_deriv # * 10
-            input_of_current_layer = X_std if 0 == layer_index else self.value_of_layer[layer_index - 1]
-            gradient_of_current_layer = input_of_current_layer.T.dot(loss_of_current_layer) / input_of_current_layer.shape[0]
-            theta_in_layer_new[layer_index] = theta_in_layer[layer_index] - self.learning_rate * gradient_of_current_layer
+            loss_of_layer = layer_of_current.calc_loss(y_result, y_std, loss_of_next_layer, theta_of_next_layer, print_log=self.print_log)
+            loss_of_next_layer = loss_of_layer
+            theta_of_next_layer = theta_of_current_layer
 
-            loss_of_next_layer = loss_of_current_layer.copy()
-
-            # 正常范围应在1e-5到1e-3之间
-            if self.print_log and (np.abs(gradient_of_current_layer).max() < 1e-5 or np.abs(gradient_of_current_layer).max() > 1e-3):
-                print(f"Layer {layer_index} gradient range:",
-                  np.abs(gradient_of_current_layer).min(),
-                  np.abs(gradient_of_current_layer).max())
-
-            grad_norm = np.linalg.norm(gradient_of_current_layer)
             self.grad_norm.append(grad_norm)
             if self.print_log:
                 print(f"Layer {layer_index} gradient norm:", grad_norm)
-
-        return theta_in_layer_new
 
     @staticmethod
     def relu_derivative(a):
@@ -367,7 +358,8 @@ class MyNeuralNetwork:
         """
         # input_features = X / 255.0  # 添加归一化
         X_std = self.standard_transformer.transform(X)
-        y_softmax = self.predict_forword(X_std, self.theta_in_layer)
+        X_input = np.insert(X_std, 0, 1, axis=1)
+        y_softmax = self.predict_forward(X_input)
         y_max_index = np.argmax(y_softmax, axis=1).reshape(-1, 1)
 
         return y_max_index
