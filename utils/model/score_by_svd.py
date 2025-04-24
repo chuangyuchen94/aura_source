59c0445526de44c7f3562878ec09acacb399bd8f
chuangyuchen94 <chuangyuchen94@gmail.com>
2025-04-24 13:54:57 +0800

    实现svn分解及预测，以作为复用

--- utils/model/score_by_svd.py
+++ utils/model/score_by_svd.py
@@ -1,24 +1,35 @@
-from sklearn.decomposition import TruncatedSVD
+from scipy.sparse.linalg import svds
 from scipy.sparse import csr_matrix
+import numpy as np
 
-class ScoringBySVD：
+class ScoringBySVD:
     """
     通过SVD矩阵分解，求得“用户-物品”的评分
     """
-    def __init__(self):
+    def __init__(self, n_components):
         """
         初始化方法
         """
-        pass
+        self.n_components = n_components
+        self.U = None # 用户潜在特征矩阵
+        self.S = None # 特征重要性矩阵
+        self.Vt = None # 物品潜在特征矩阵的转置
 
-    def fit(self, data):
+    def fit(self, X, y=None):
         """
         将传入的数据，分解为U、S、V三个矩阵
         """
-        pass
+        U, s, Vt = svds(X, self.n_components)
+        S = np.diag(S)
 
-    def predict(self, feature):
+        self.U = U
+        self.S = S
+        self.Vt = Vt
+
+    def predict(self, X):
         """
         预测评分
         """
-        pass
+        scores = self.U[X] @ self.S @ self.Vt
+
+        return scores
